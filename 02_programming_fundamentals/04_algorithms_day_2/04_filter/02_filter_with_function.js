2f9eb0153ac9b3bb25878e8c64b6715e82c3795d
Damien LEPRETRE <damien.lepretre@decathlon.com>
2017-11-24 15:45:33 +0100

    exe4 - filters

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,7 +1,22 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
 function filter(array, fn) {
-  // Your code here
+  let newArray= [];
+  if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      if (fn(array[i])) {
+        newArray.push(array[i]);
+      }
+    }
+  }
+  return newArray;
+}
+function isEven(number) {
+  return (number % 2 === 0);
+}
+
+function isOdd(number) {
+  return (number % 2 !== 0);
 }
 
 // do not remove this line, it is for tests
--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
@@ -2,6 +2,21 @@
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
 function filter(array, fn) {
   // Your code here
+  let newArray= [];
+  if (array.length > 0) {
+    return array.filter(number => fn(number));
+  } else {
+    return [];
+  }
+
+}
+
+function isEven(number) {
+  return (number % 2 === 0);
+}
+
+function isOdd(number) {
+  return (number % 2 !== 0);
 }
 
 // do not remove this line, it is for tests
