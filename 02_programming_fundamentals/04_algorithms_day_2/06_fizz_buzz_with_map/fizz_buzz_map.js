b40860b29a82fb4c2354f1fe55c581c8aa0ff29f
Damien LEPRETRE <damien.lepretre@decathlon.com>
2017-11-24 16:18:12 +0100

    exe4 - fizz map

--- 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
+++ 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
@@ -10,7 +10,19 @@
 */
 
 function fizzBuzz(list) {
+  return list.map(getWord);
+}
 
+function getWord(number) {
+  if ((number % 5 === 0 && number % 3 === 0)) {
+    return "FizzBuzz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else {
+    return number;
+  }
 }
 
 
