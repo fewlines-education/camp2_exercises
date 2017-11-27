fb92eefb00bf6ecab98859b44ea64e366bf461e9
Damien LEPRETRE <damien.lepretre@decathlon.com>
2017-11-27 13:28:56 +0100

    square digits

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -7,9 +7,27 @@
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
 
-function squareDigits(number) {
-  // Your code here
+const square = (number) => (number * number).toString();
+
+function squareDigits(number, callback) {
+  let sqDigits = "";
+
+  // convertir int => string
+  const numberInString = number.toString();
+
+  // récupérer la longueur du string
+  const numberLength  = numberInString.length;
+
+  // pour chaque digit concat le chiffre trouvé (appel fonction spéciale)
+  for (let i = 0; i < numberLength; i++) {
+    sqDigits = sqDigits + square(Number(numberInString[i]));
+  }
+
+  // callback la concat avec le convert string => int
+  return Number(sqDigits);
 }
+
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
