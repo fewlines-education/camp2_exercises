b0988880b9b6c55d3c60a4b0026b4a178a2bc10f
Damien LEPRETRE <damien.lepretre@decathlon.com>
2017-11-30 14:34:31 +0100

    orangeTree

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -6,10 +6,35 @@ const orangeTree = {
   oranges: 0,
   alive: false,
   pickAnOrange: function() {
-    return 0;
+    if(this.oranges > 0) {
+      this.oranges = this.oranges - 1
+      return true
+    } else {
+        return false;
+    }
   },
   ageOneYear: function() {
-    return 0;
+    this.age += 1;
+    if (this.age < 10) {
+      this.height += 25; // 25 centimeters until it is 10 years old.
+      if(this.age >= 5) {
+        this.oranges = 10; // 10 oranges when it's between 5 and 10 years old.
+        return this
+      }
+    } else if (this.age >= 10 && this.age < 20) {
+      this.height += 10; //10 centimeters until it is 20 years old
+      this.oranges = 20 // 20 oranges when it's between 10 and 20 years old
+      return 10;
+    } else if (this.age >= 20 && this.age < 40) {
+      this.oranges = 5 // 5 oranges until it's 40 years old.
+      return this;
+    } else if (this.age >= 40 && this.age < 50) {
+      this.oranges = 0;
+    } else if (this.age >= 50) {
+      this.alive = RIPSoon(this.age);
+      return this;
+    }
+
   },
   seed: function() {
     this.height = 0;
@@ -19,5 +44,16 @@ const orangeTree = {
     return this;
   }
 }
+
+function RIPSoon(age) {
+  const deadAge = Math.floor(Math.random() * 100 + 1);
+  if (deadAge >= age && deadAge <= 100 ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 
 module.exports = orangeTree;
