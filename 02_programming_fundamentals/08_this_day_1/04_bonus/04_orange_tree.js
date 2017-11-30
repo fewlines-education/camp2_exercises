ffc497aa9edf8130d977cc8c618e948ab520b633
Damien LEPRETRE <damien.lepretre@decathlon.com>
2017-11-30 13:29:45 +0100

    orange tree wip1

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -1,3 +1,23 @@
 // See Sparta courses for the exercise summary
 
+const orangeTree = {
+  height: 0, // in cm
+  age: 0, // in years
+  oranges: 0,
+  alive: false,
+  pickAnOrange: function() {
+    return 0;
+  },
+  ageOneYear: function() {
+    return 0;
+  },
+  seed: function() {
+    this.height = 0;
+    this.age = 0;
+    this.oranges = 0;
+    this.alive = true;
+    return this;
+  }
+}
+
 module.exports = orangeTree;
