8a1b214d8a2f9ec07f992fc0a52114a3b1dd0d83
Martin Vielvoye <martin.vielvoye@gmail.com>
2018-04-08 11:05:34 +0200

    "finished the bonus exercices with correction in tests for fibonacci"

--- 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/01_factorial.js
+++ 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/01_factorial.js
@@ -6,9 +6,12 @@
 //
 // Remember that you can call `fact` inside of itself
 
-
 function fact(n) {
-  
+  if(n<0){return null;}
+  else if(!Number.isInteger(n)){return null;}
+  else if(n === 0){return 1;}
+  else if(n === 1){return n;}
+  else {return n * fact(n-1);}
 }
 
 
--- 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/03_pow.js
+++ 02_programming_fundamentals/04_algorithms_day_2/08_recursion_bonus/03_pow.js
@@ -5,7 +5,21 @@
 // for instance pow(2,8) = 256
 
 function pow(number, power) {
-  // Your code here
+  if(power === 0){return 1;}
+  else if(!Number.isInteger(number)){return null;}
+  else if(number === 0){return number;}
+  else {
+    let result = number;
+
+    for(let i = 1; i < power; i++){
+      result = mult(result,number);
+    }
+    return result;
+  }
+}
+
+function mult(number, number2){
+  return number * number2;
 }
 
 // do not remove this line, it is for tests
