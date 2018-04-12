cfb1748f5955c4f46daa51b31fd51287b9e5b25c
Martin Vielvoye <martin.vielvoye@gmail.com>
2018-04-12 13:47:08 +0200

    exercices of the day with all test valid

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,34 @@
 // See Sparta courses for the exercise summary
+const car = {
+  //Status
+  speed: 0,
+  timeTraveled: 0,
+  distancetraveled: 0,
 
+  //Method
+  start: function(){
+    this.speed = 0;
+    this.timeTraveled = 0;
+    this.distancetraveled = 0;
+    return this;
+  },
+
+  changeSpeed: function(speed){
+    this.speed = speed;
+    return this;
+  },
+
+  drive: function(time){
+    this.timeTraveled = time;
+    this.distancetraveled = this.distancetraveled + (this.timeTraveled * (this.speed / 60));
+    return this;
+  },
+
+  showDistance : function(){
+    console.log(this.distancetraveled + " Km");
+    return this.distancetraveled + " Km";
+  }
+};
+
+console.log(car.start().changeSpeed(130).drive(42).showDistance());
 module.exports = car;
--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,14 +1,40 @@
 // See Sparta courses for the exercise summary
 
 // Coffee Machine usage. Insert your code above this comment
+const machine = {
+  //Status
+  litersOfCoffee : 0,
+
+  //Method
+  fillWithLitersOfCoffee: function(liters){
+    this.litersOfCoffee = this.litersOfCoffee + liters;
+  },
+
+  expresso: function(){
+    if(this.litersOfCoffee >= 0.08){
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    }
+    else{return false;}
+  },
+
+  longCoffee: function(){
+    if(this.litersOfCoffee >= 0.15){
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    }
+    else{return false;}
+  },
+};
+
 
 machine.fillWithLitersOfCoffee(10);
-console.log(machine.expresso()) // => true
-console.log(machine.litersOfCoffee) // => 9.92
-console.log(machine.longCoffee()) // => true
-console.log(machine.litersOfCoffee) // => 9.77
+console.log(machine.expresso()); // => true
+console.log(machine.litersOfCoffee); // => 9.92
+console.log(machine.longCoffee()); // => true
+console.log(machine.litersOfCoffee); // => 9.77
 // some more people ordering coffee here
-console.log(machine.litersOfCoffee) // => 0.02
-console.log(machine.expresso()) // => false
+console.log(machine.litersOfCoffee); // => 0.02
+console.log(machine.expresso()); // => false
 
 module.exports = machine;
