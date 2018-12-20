2fb0d5530f5bdb7125accc6f6f5e0d30058c54b8
Martin Vielvoye <martin.vielvoye@gmail.com>
2018-12-20 11:18:44 +0100

    changed test file name and corrected skeleton for errors

--- 03_backend_developement/03_testing_our_code/03_going_further_with_tests/going_further_with_tests.test.js
+++ 03_backend_developement/03_testing_our_code/03_going_further_with_tests/going_further_with_tests.test.js
@@ -1,10 +1,12 @@
+/* global beforeEach */
+
 let mockArray = [];
 let mockObject = {};
 
 beforeEach(() => {
   mockArray = [];
   mockObject = {};
-})
+});
 
 describe("Let the tests on arrays begin !", () => {
 
@@ -12,19 +14,19 @@ describe("Let the tests on arrays begin !", () => {
   //Be sure to always test if you test passes with the right conditions but also if it does fail with the wrong conditions.
 
   //Create a test that ensures that "Alien the 8th passenger" is present in mockArray
-  mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"]
+  mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
 
   //Create a test that fails if "Indiana Jones 4" is present in mockArray
-  mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"];
+  mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
 
   //Create a test fails if the list has less than 5 elements
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger",  "Tombeau des lucioles", "fight club"];
+  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
 
   //Create a test that checks if the array only contains String element
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger",  "Tombeau des lucioles", "fight club"];
+  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
 
   //Create a test that passes if there's at least one film with "lord of the rings" inside mockArray
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger",  "Tombeau des lucioles", "fight club"];
+  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
 
 });
 
@@ -36,12 +38,12 @@ describe("Let the tests on objects begin !", () => {
     fries: true,
     kebab: "Double",
     soda: "Sprite",
-  }
+  };
 
   //Create a test that checks if the elements in mockArray respect that the strings are the same with the right order as solutionArray
   //eg : mockArray = ["Blue","Blue","Red", "Yellow"] would fail the test
   mockArray = ["Blue", "Red", "Blue", "Yellow"];
-  solutionArray = ["Blue", "Red", "Blue", "Yellow"];
+  const solutionArray = ["Blue", "Red", "Blue", "Yellow"];
 });
 
 //At the end, your file should contain 7 test.
