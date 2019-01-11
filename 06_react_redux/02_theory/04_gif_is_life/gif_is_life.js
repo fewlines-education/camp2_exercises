5145c64190ba94f96d2058681fbd9eddbcbdcc94
Martin Vielvoye <martin.vielvoye@gmail.com>
2019-01-11 10:14:30 +0100

    Updated format files of ex for correct react initiation, verified test passed

--- 06_react_redux/02_theory/04_gif_is_life/gif_is_life.js
+++
@@ -1,1 +0,0 @@
-//Should the giphy api be included for the students not to have to go through this part and focus on the "react" part ?
---
+++ 06_react_redux/02_theory/04_gif_is_life/src/App.js
@@ -0,0 +1,16 @@
+//Should the giphy api be included for the students not to have to go through this part and focus on the "react" part ?
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Write Your Gif Code Here !
+      </div>
+    )
+  }
+}
+
+export default App;
