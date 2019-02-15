2fe709a38d4f7bf19b149c30c5bfdad4869324af
Martin Vielvoye <martin.vielvoye@gmail.com>
2019-02-15 18:01:05 +0100

    todo list new exercise

--- 06_react_redux/07_redux/03_to_do/src/App.test.js
+++ 06_react_redux/07_redux/03_to_do/src/App.test.js
@@ -1,62 +1,91 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
-import Enzyme,{shallow} from 'enzyme'
+import Enzyme,{shallow, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import Connected, {App} from './App';
+import store from "./store";
+const Wapiti = require("wapiti");
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <connected />
+//   </Provider>,
+//   document.getElementById('root')
+// )
 
 Enzyme.configure({ adapter: new Adapter() })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let storeMock = store
+
+describe('tests', () => {
+
+  afterEach(() => {
+    console.log(store);
+    storeMock = store
+  });
+
+
+  test('the list has 0 elements long at the beginning', () => {
+    const listMock = mount(<Provider store={storeMock}>
+      <Connected />
+    </Provider>);
+
+    expect(listMock.find('li')).toHaveLength(0)
+  })
+
+  test("a button exists", () => {
+    const listMock = mount(<Provider store={storeMock}>
+      <Connected />
+    </Provider>);
+
+    console.log(listMock.find('button').text());
+
+    expect(listMock.find('button').text()).toBe("Submit");
+    expect(listMock.find('button').length).toBe(1);
+  })
+
+  test("li has one empty element after submitting an empty form", () => {
+    const listMock = mount(<Provider store={storeMock}>
+      <Connected />
+    </Provider>);
+
+    expect(listMock.find('li')).toHaveLength(0);
+
+
+    listMock.find('button').simulate('click');
+    console.log(listMock.find('li'));
+    console.log(listMock.find('li').text());
+
+    expect(listMock.find('li')).toHaveLength(1);
+  })
+
+  test("'Potato' is present in the list after filling it in in the form", async () => {
+
+    const listMock = mount(<Provider store={storeMock}>
+      <Connected />
+      </Provider>
+    );
+
+    const input = listMock.find('input');
+
+    input.simulate('change', { target: { value: 'Potato' } })
+
+    // console.log("button one : ", listMock.find('button').at(0).text());
+
+    listMock.find('button').at(0).simulate('click');
+
+    // console.log("first element is : ", firstElement);
+    // console.log("first element is : ", listMock.find('li').text());
+    console.warn("listmock looks like : ",listMock.find('li').first().text())
+    let mockLi = listMock.find('li')
+    // console.warn("listmock looks like : ",listMock.find('li').at(1).text())
+
+    expect(mockLi).toHaveLength(2)
+    expect(mockLi.first().text()).not.toMatch(/Potato/)
+    expect(mockLi.at(1).text()).toMatch(/Potato/)
+    expect(listMock.find('img')).toHaveLength(2)
+
+  });
 });
-
-it('the list is 10 elements long', () => {
-  const listMock = shallow(<App/>);
-
-  expect(listMock.find('li')).toHaveLength(10)
-})
-
-it("a button exists", () => {
-  const listMock = shallow(<App/>);
-
-  expect(listMock.find('button')).toBeDefined();
-})
-
-it("the first printed list is in ascending order", () => {
-  const listMock = shallow(<App/>);
-
-  let beforeClickElements = listMock.find('li').getElements();
-  let beforeClick = beforeClickElements.map(element => element.props.children)
-
-  let arrayMock  = [0,1,2,3,4,5,6,7,8,9];
-
-  expect(beforeClick).toMatchObject(arrayMock)
-});
-
-it('List changes after clicking on the button', () => {
-  const listMock = shallow(<App/>);
-
-  let beforeClickElements = listMock.find('li').getElements();
-  let beforeClick = beforeClickElements.map(element => element.props.children)
-
-  listMock.find('button').simulate('click');
-
-  let afterClickElements = listMock.find('li').getElements();
-  let afterClick = afterClickElements.map(element => element.props.children)
-
-  expect(beforeClick).not.toMatchObject(afterClick)
-})
-
-it('List is reversed after clicking on the button', () => {
-  const listMock = shallow(<App/>);
-  let arrayMockReversed  = [9,8,7,6,5,4,3,2,1,0];
-
-  listMock.find('button').simulate('click');
-
-  let afterClickElements = listMock.find('li').getElements();
-  let afterClick = afterClickElements.map(element => element.props.children)
-
-  expect(afterClick).toMatchObject(arrayMockReversed)
-})
--- 06_react_redux/07_redux/03_to_do/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import toDo from './reducers';
-
-const store = createStore(toDo);
-
-export default store;
