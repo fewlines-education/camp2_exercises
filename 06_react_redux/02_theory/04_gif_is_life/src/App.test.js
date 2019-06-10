3ceb52802fcc1ad8dcc038acf2a2fbc99707d69e
Vincent Billey <Fenntasy@users.noreply.github.com>
2019-06-10 11:50:10 +0200

    Bumping deps and removing old and unused files (#106)

--- 06_react_redux/02_theory/04_gif_is_life/src/App.test.js
+++ 06_react_redux/02_theory/04_gif_is_life/src/App.test.js
@@ -1,53 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme,{shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
-const Wapiti = require("wapiti");
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
+Enzyme.configure({ adapter: new Adapter() });
 
-Enzyme.configure({ adapter: new Adapter() })
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
+test("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-test('an input component is present', () => {
-  const searchMock = shallow(<App/>);
+test("an input component is present", () => {
+  const searchMock = shallow(<App />);
 
-  expect(searchMock.find('input')).toBeDefined();
-})
+  expect(searchMock.find("input")).toBeDefined();
+});
 
 test("a button exists", () => {
-  const searchMock = shallow(<App/>);
+  const searchMock = shallow(<App />);
 
-  expect(searchMock.find('button')).toBeDefined();
-})
+  expect(searchMock.find("button")).toBeDefined();
+});
+
+const waitFor = time => new Promise(resolve => setTimeout(resolve, time));
 
 test("keyword filled in form gives the right gif's URL from GIPHY", async () => {
-
-  const searchedGif = await Wapiti().goto("http://localhost:3000")
-    .fillForm({"#gifSearcher": "lol"}, { waitForPageLoad: false })
-    .puppeteer(page => page.waitForSelector("img"))
-    .capture(() => {
-      return document.querySelector("img").src
-    })
-    .run()
+  const searchMock = mount(<App />);
+  searchMock
+    .find('[type="text"]')
+    .simulate("change", { target: { value: "lol" } });
+  searchMock.find("form").simulate("submit");
+  await waitFor(5000);
+  searchMock.update();
+  const searchedGif = searchMock
+    .find("img")
+    .first()
+    .props().src;
 
   const awaitedGifUrl = await fetch(
     `http://api.giphy.com/v1/gifs/search?q=lol&api_key=dTJ7Uasn35PekyE6Vk8lix4AZEFmSEw4&limit=1`,
-    {method: "GET"}
+    { method: "GET" }
   )
-    .then((response) => response.json())
-    .then((result) => {
+    .then(response => response.json())
+    .then(result => {
       return result.data[0].images.original.url;
     })
-    .catch((error) => {
+    .catch(error => {
       console.warn(error);
-    })
+    });
 
-  expect(searchedGif).toBe(awaitedGifUrl)
-
+  expect(searchedGif).toBe(awaitedGifUrl);
 }, 10000);
